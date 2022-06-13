ac10a0b0972c15c90f1381f6f22ce8b531b48578
ccomkhj <ccomkhj@gmail.com>
2022-06-13 09:42:16 +0200

    area module for airflow

---
+++ api/airflow_compute_area.py
@@ -0,0 +1,17 @@
+import sys
+import argparse
+
+def parse_args():
+    '''Parse input arguments'''
+    parser = argparse.ArgumentParser(
+                description="Compute area of plants")
+    parser.add_argument("image", 
+                        nargs='+', 
+                        type=str, 
+                        help="images to be processed.")
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(args.image)
