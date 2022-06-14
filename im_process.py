bf19fbceae010fa14e607804d5daefb92eba4a29
ccomkhj <ccomkhj@gmail.com>
2022-06-14 09:57:19 +0200

    area shape update

--- im_process.py
+++ im_process.py
@@ -69,9 +69,7 @@ def compute_area_api(images):
         hexa.load_img(filepath=img_full_path, metapath=METAPATH, separator=SEPARATOR)
         hexa.undistort().segment_with_model(show=False, pallete_path=None).compute_area().document(areas, graph=False, volume=False)
 
-    """ convert list to SQL format """
-    output = ',' .join(list(map(lambda x: "('" + x[0] + "', " + str(x[1]) +  ")", areas)))
-    return output
+    return areas
 
 def compute_area(args, include_header = False):
     METAPATH = args.meta
