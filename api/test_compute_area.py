abdb59f929ff66adb099ca6acb3b8c7c11a37a09
ccomkhj <ccomkhj@gmail.com>
2022-07-18 12:24:16 +0200

    update testing code for docker

---
+++ api/test_compute_area.py
@@ -0,0 +1,38 @@
+"""
+Compute growth area for API.
+
+Author: Huijo Kim
+Email: huijo.k@hexafarms
+Version: V1.0
+"""
+import argparse
+import os
+import sys
+
+from im_process import compute_area_api
+
+sys.path.append(os.getcwd())
+
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description="Compute area of plants")
+    parser.add_argument("image",
+                        nargs='+',
+                        type=str,
+                        help="images to be processed.")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    images = args.image
+    meta_path = "hexa_meta.json"
+    img_path = "demo/undistort_demo.jpg"
+    
+    area = compute_area_api(images, METAPATH=meta_path, IMGFILE_DIR=img_path)
+    if area > 0:
+        print("Test is Successful!.")
+    
