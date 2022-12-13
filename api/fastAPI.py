39292df441ec9abfeb967db4027097b13293989c
huijo <ccomkhj@gmail.com>
2022-12-13 09:44:41 +0100

    fast api apply

--- api/fastAPI.py
+++ api/fastAPI.py
@@ -1,12 +1,12 @@
-from fastapi.responses import Response
-from fastapi import FastAPI, UploadFile, File, Query
-from predict import segment_api as segment
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+import boto3
 import os
-import io
-import re
 from pathlib import Path
 from openHexa.utils.helpers import getNewVersion
+from tools.process import compute_area_api
+from configs.aws import prepare_configs
 
 app = FastAPI()
 
@@ -30,7 +30,13 @@ async def sync_instantSeg(location: str):
     if location is None:
         return {"Warning": "Location is not provided!"}
 
-    weightDir = "/weights/mmdet"
+    mode = "mmdet"
+
+    weightDir = os.path.join("/weights", mode)
+
+    # Download config, weight, meta file.
+    s3_client=prepare_configs(mode)
+    
     newVersion = getNewVersion(weightDir)
 
     config_file = os.path.join(weightDir, f"v{str(newVersion)}", "config.py")
@@ -39,8 +45,19 @@ async def sync_instantSeg(location: str):
     imgDir = os.path.join("/images", location)
     Path(imgDir).mkdir(parents=True, exist_ok=True)
 
+    imgsInS3 = set([i['Key'] for i in s3_client.list_objects(Bucket=location)['Contents']])
+
     # TODO: get files names from S3
     # TODO: get file names at the location from RDS (uhm... should I include RDS here???)
-    # TODO: S3 - RDS => Download to local
+    # TODO: S3 - RDS - local => Download to local
     # TODO: Compute areas of files, and then update to RDS (or return to Airflow..??? is it safe??)
     # Maybe in this repo, only read permission to DB is awarded, and send the area info to Airflow? Then it can be micro-controlled, and safe!.
+
+
+    imgs = [] # should be computed
+
+    areas = compute_area_api(
+        imgs, newVersion, METAPATH = "/meta/hexa_meta.json", IMGFILE_DIR= imgDir, mode= mode)
+    
+    return ORJSONResponse(areas)
+    
--- configs/aws.py
+++ configs/aws.py
@@ -5,13 +5,20 @@ import os
 from pathlib import Path
 from typing import Dict
 
-def _connect_aws_s3(AWS_KEY:str, AWS_SECRET:str, AWS_REGION:str):
+
+def _connect_aws_s3(AWS_KEY: str, AWS_SECRET: str, AWS_REGION: str):
     """Connect to aws client"""
+
+    # Get credentials to access to S3 bucket.
+    AWS_KEY = os.environ["AWS_KEY"]
+    AWS_SECRET = os.environ["AWS_SECRET"]
+    AWS_REGION = os.environ["AWS_REGION"]
+
     s3_client = boto3.client(
         "s3",
         region_name=AWS_REGION,
         aws_access_key_id=AWS_KEY,
-        aws_secret_access_key=AWS_SECRET
+        aws_secret_access_key=AWS_SECRET,
     )
     return s3_client
 
@@ -28,17 +35,13 @@ def _check_bucket(s3_client, bucket_name):
 
     return bucket
 
-def prepare_configs(setup:Dict, cv_mode:str):
+
+def prepare_configs(cv_mode: str):
     """Prepare configs for segmentation."""
 
     assert cv_mode in ["mmseg", "mmdet"], f"Mode is inappropriate. Input: {cv_mode}"
 
-    # Get credentials to access to S3 bucket.
-    AWS_KEY = os.environ['AWS_KEY']
-    AWS_SECRET = os.environ['AWS_SECRET']
-    AWS_REGION = os.environ['AWS_REGION']
-
-    s3_client = _connect_aws_s3(AWS_KEY, AWS_SECRET, AWS_REGION)
+    s3_client = _connect_aws_s3()
 
     "Check the bucket is inside AWS S3"
     blink_bucket_name = _check_bucket(s3_client, "configs-and-weights")
@@ -46,38 +49,42 @@ def prepare_configs(setup:Dict, cv_mode:str):
 
     """Find the newest version"""
     relevant_versions = []
-    
-    new_version = 0
-    for j in s3_client.list_objects(Bucket=blink_bucket_name)['Contents']:
-        if j['Key'].split('/')[0]==cv_mode:
-            relevant_versions.append(j)
-            if re.search('\/v(.*)\/', j['Key'], 0) is not None:
-                version = int(re.search('\/v(.*)\/', j['Key'], 0).group(1))
-                if  version > new_version:
-                    new_version = version 
 
-    up2date_config_v = f'v{new_version}'                
+    new_version = 0
+    for j in s3_client.list_objects(Bucket=blink_bucket_name)["Contents"]:
+        if j["Key"].split("/")[0] == cv_mode:
+            relevant_versions.append(j)
+            if re.search("\/v(.*)\/", j["Key"], 0) is not None:
+                version = int(re.search("\/v(.*)\/", j["Key"], 0).group(1))
+                if version > new_version:
+                    new_version = version
+
+    up2date_config_v = f"v{new_version}"
 
     """Check if configs and weights are in volumes"""
-    config_file_path = os.path.join('/weights', cv_mode, up2date_config_v, 'config.py')
-    weights_path = os.path.join('/weights', cv_mode, up2date_config_v, 'weights.pth')
+    config_file_path = os.path.join("/weights", cv_mode, up2date_config_v, "config.py")
+    weights_path = os.path.join("/weights", cv_mode, up2date_config_v, "weights.pth")
     Path(config_file_path).parents[0].mkdir(parents=True, exist_ok=True)
-    meta_path = os.path.join('/openHexa', 'meta', 'hexa_meta.json')
+
+    meta_path = os.path.join("/meta", "hexa_meta.json")
     Path(meta_path).parents[0].mkdir(parents=True, exist_ok=True)
 
-    """MetaData is downloaded everytime."""            
+    """MetaData is downloaded everytime."""
     aws_meta_path = os.path.join("camera", "hexa_meta.json")
     s3_client.download_file(meta_bucket_name, aws_meta_path, str(meta_path))
     logger.info("Meta file for image segmentation is downloaded.")
-    
+
     """Download if not the new version exist."""
     if not os.path.exists(config_file_path):
         aws_config_path = os.path.join(cv_mode, up2date_config_v, "config.py")
-        s3_client.download_file(blink_bucket_name, aws_config_path, str(config_file_path))
+        s3_client.download_file(
+            blink_bucket_name, aws_config_path, str(config_file_path)
+        )
         logger.info("Config file for image segmentation is downloaded.")
-    
+
     if not os.path.exists(weights_path):
         aws_weights_path = os.path.join(cv_mode, up2date_config_v, "weights.pth")
         s3_client.download_file(blink_bucket_name, aws_weights_path, str(weights_path))
         logger.info("Weights file for image segmentation is downloaded.")
-    
+
+    return s3_client
